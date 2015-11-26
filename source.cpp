66a8cc4b36ac4c7183e7685fd25f1c97186fed8d
BBarbosa <brunobarbosa.caos@gmail.com>
2015-11-26 23:58:38 +0000

    papi not working

--- source.cpp
+++ source.cpp
@@ -3,16 +3,24 @@
 #include <cstdlib>
 #include <vector>
 #include <stdio.h>
+#include "papi.h"
 
 using namespace std;
 
 #define SIZE 10
 #define REPETITIONS 1
+#define NUM_EVENTS 2
 
 float m1[SIZE][SIZE],m2[SIZE][SIZE],result[SIZE][SIZE];
 
+long long values[NUM_EVENTS];
+int Events[NUM_EVENTS] = {PAPI_TOT_INS,PAPI_TOT_CYC};
+int EventSet;
+int retval;
+
 void matrixMult(float** mat1, float** mat2, float** res){
     int i,j,k;
+
     for(i=0;i<SIZE;i++)
     {
         for(j=0;j<SIZE;j++)
@@ -28,9 +36,9 @@ void matrixMult(float** mat1, float** mat2, float** res){
 void fillMatrices(void) {
     for (unsigned i = 0; i < SIZE; ++i) {
     	  for (unsigned j = 0; j < SIZE; ++j) {
-			  m1[i][j] = ((float) rand()) / ((float) RAND_MAX);
-			  m2[i][j] = ((float) rand()) / ((float) RAND_MAX);
-			  result[i][j]  = 0;
+			       m1[i][j] = ((float) rand()) / ((float) RAND_MAX);
+			       m2[i][j] = ((float) rand()) / ((float) RAND_MAX);
+			       result[i][j]  = 0;
 		}
 	}
 }
@@ -48,6 +56,10 @@ int main(int argc, char *argv[]){
   float **mat1, **mat2, **res;
 	long long unsigned time;
 
+  retval = PAPI_library_init(PAPI_VER_CURRENT);
+  retval = PAPI_create_eventset(&EventSet);
+  retval = PAPI_add_events(EventSet,Events,NUM_EVENTS);
+
 	mat1 = new float*[SIZE];
 	mat2 = new float*[SIZE];
 	res = new float*[SIZE];
@@ -55,7 +67,7 @@ int main(int argc, char *argv[]){
 	for (unsigned i = 0; i < SIZE; ++i) {
 		mat1[i] = new float[SIZE];
 		mat2[i] = new float[SIZE];
-		res[i] = new float[SIZE];	
+		res[i] = new float[SIZE];
 	}
 
 	fillMatrices();
@@ -64,19 +76,18 @@ int main(int argc, char *argv[]){
 		for (unsigned j = 0; j < SIZE; ++j) {
 			mat1[i][j] = m1[i][j];
 			mat2[i][j] = m2[i][j];
-      		result[i][j] = res[i][j];
+      result[i][j] = res[i][j];
 		}
 	}
 
 	double *clearcache;
-
 	for (unsigned i = 0; i < REPETITIONS; i++) {
 		free(clearcache);
 		clearcache = new double [3300000];
 
-		//start();
+		PAPI_start_counters((int*)Events,NUM_EVENTS);
 		matrixMult(mat1, mat2, res);
-		//stop();
+		int retval = PAPI_stop_counters(values,NUM_EVENTS);
 	}
   printMatrice(res);
 
