a472d154b09ed3d64bfbb5d756ae3cc80950cdd2
BBarbosa <brunobarbosa.caos@gmail.com>
2015-11-27 12:24:27 +0000

    just a commit

--- sourcePapi.cpp
+++ sourcePapi.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-#define SIZE 1000
+#define SIZE 10
 #define REPETITIONS 2
 #define NUM_EVENTS 4
 
@@ -27,34 +27,22 @@ void printEvents(int rep) {
 	printf("PAPI_SR_INS: %lld\n",values[1]);
 	printf("PAPI_LD_INS: %lld\n",values[2]);
 	printf("PAPI_BR_INS: %lld\n",values[3]);
-	printf("PAPI_VEC_SP: %lld\n",values[4]);
-	/*
-	printf("PAPI_VEC_DP: %d\n",values[5]);
-	
-	printf("PAPI_FP_OPS: %d\n",values[6]);
-	printf("PAPI_SP_OPS: %d\n",values[7]);
-	printf("PAPI_DP_OPS: %d\n",values[8]);
-	printf("PAPI_TOT_INS: %d\n",values[9]);
-	
-	printf("PAPI_L1_TCM: %d\n",values[10]);
-	printf("PAPI_L2_TCM: %d\n",values[11]);
-	printf("PAPI_L1_TCA: %d\n",values[12]);
-	printf("PAPI_L2_TCA: %d\n",values[13]);
-	printf("PAPI_TOT_CYC: %d\n\n",values[14]);
-	*/
 }
 
 void matrixMult(float** mat1, float** mat2, float** res){
     int i,j,k;
+		float sum;
 
     for(i=0;i<SIZE;i++)
     {
         for(j=0;j<SIZE;j++)
         {
+						sum = 0;
             for(k=0;k<SIZE;k++)
             {
-                res[i][j] += mat1[i][k] * mat2[k][j];
+                sum += mat1[i][k] * mat2[k][j];
             }
+						res[i][j] = sum;
         }
     }
 }
@@ -95,31 +83,32 @@ int main(int argc, char *argv[]){
 		mat2[i] = new float[SIZE];
 		res[i] = new float[SIZE];
 	}
-	
+
 	fillMatrices();
-	
+
 	for (unsigned i = 0; i < SIZE; ++i){
 		for (unsigned j = 0; j < SIZE; ++j) {
 			mat1[i][j] = m1[i][j];
-			mat2[i][j] = m2[i][j];
-      			result[i][j] = res[i][j];
+			//mat2[i][j] = m2[i][j];
+			mat2[j][i] = m2[i][j]; //trasnpose
+      result[i][j] = res[i][j];
 		}
 	}
-	
+
 	double *clearcache = NULL;
 	for (unsigned i = 0; i < REPETITIONS; i++) {
 		if(clearcache != NULL) {
 			free(clearcache);
 		}
 		clearcache = new double [3300000];
-		
+
 		PAPI_start_counters(Events,NUM_EVENTS);
 		matrixMult(mat1, mat2, res);
 		int retval = PAPI_stop_counters(values,NUM_EVENTS);
-		
+
 		printEvents(i);
 	}
   	//printMatrice(res);
-	
+
 	return 1;
 }
